be42bf82801c617b9aec2e99b5467d5b05fcd964
tampham92 <84772571+tampham92@users.noreply.github.com>
2021-09-28 14:09:45 +0700

    Update AccountController.js

--- Git/App-QLSV/src/app/controllers/AccountController.js
+++ Git/App-QLSV/src/app/controllers/AccountController.js
@@ -16,39 +16,47 @@ class AccountController {
         const username = req.body.username
         const password = req.body.password
         const role = req.body.role
-        Account.findOne({
-            username: username,
-            email: email
-        })
-        .then(data => {
-            if(data) {
-                res.json({
-                    result: 0,
-                    message: 'Tai khoan da ton tai'
-                })
-            } else{
-                return Account.create({
-                    name: name,
-                    email: email,
-                    username: username,
-                    password: password,
-                    role: role
-                })
-            }   
-        })   
-        .then((data) => {
-            console.log(data)
+
+        if (!name || !email || !username || !role){
             res.json({
-                result: 1,
-                message: data
-            })
-        })
-        .catch(() => {
-            res.status(500).json({
                 result: 0,
-                message: 'Tao tai khoan thất bại'
+                message: 'Chưa điền đủ thông tin'
             })
-        })
+        } else {
+            Account.findOne({
+                username: username,
+                email: email
+            })
+            .then(data => {
+                if(data) {
+                    res.json({
+                        result: 0,
+                        message: 'Tai khoản tồn tại'
+                    })
+                } else{
+                    return Account.create({
+                        name: name,
+                        email: email,
+                        username: username,
+                        password: password,
+                        role: role
+                    })
+                }   
+            })   
+            .then((data) => {
+                console.log(data)
+                res.json({
+                    result: 1,
+                    message: data
+                })
+            })
+            .catch(() => {
+                res.status(500).json({
+                    result: 0,
+                    message: 'Tạo tài khỏan thất bại'
+                })
+            })
+        }
     }
 
 
